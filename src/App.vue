dc4858890397df174950694acc0f94b2743f65c4
Mateusz Sobczyk <sobczyk.m@outlook.com>
2021-05-24 14:55:41 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <TestForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TestForm from './components/TestForm.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    TestForm
   }
 }
 </script>
@@ -24,5 +20,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #f4f1de;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import VueCountdown from '@chenfengyuan/vue-countdown';
+Vue.component(VueCountdown.name, VueCountdown);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
